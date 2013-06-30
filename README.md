fbad5a66d6e2a182a83a02a243fe3081f1d3730b
Dan F-M <danfm@nyu.edu>
2013-06-30 18:57:40 -0400

    start fresh for julython

---
+++ README.md
@@ -0,0 +1,3 @@
+A repository for [julython](http://www.julython.org/).
+
+Papers by Dan Foreman-Mackey (c) 2013
--- stripe82/queries/fields.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    p.run,p.camcol,p.field,p.rerun,p.mjd_g,p.ramin,p.ramax,p.decmin,p.decmax
-INTO mydb.fields
-FROM Stripe82..Field AS p
-WHERE p.mjd_g > 0
-
